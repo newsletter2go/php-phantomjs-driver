f42bab90fe4811bb37c76c3fa5895b6afad1fdd2
Niels Hoppe <hoppe@newsletter2go.com>
2015-08-27 10:08:23 +0200

    Refactor

--- lib/PhantomDriver.php
+++ lib/PhantomDriver.php
@@ -1,6 +1,6 @@
 <?php
 
-//namespace HielsNoppe\PhantomJS;
+namespace NielsHoppe\PhantomJS;
 
 /**
  *
@@ -14,22 +14,22 @@ class PhantomDriver {
 
     function __construct () {
 
-        $this->running = false;
+        $this->process = new PhantomProcess();
+    }
+
+    public function start ($script) {
+
+        $this->process->start();
+    }
+
+    public function stop () {
+
+        return $this->process->stop();
     }
 
     public function process ($url) {
 
-        if (!$this->running) {
-
-            // TODO: Throw an exception
-            return;
-        }
-
-        // send URL to PhantomJS process
-        fwrite($this->pipes[0], "$url\n");
-
-        // read back result from PhantomJS process
-        $result = fgets($this->pipes[1]);
+        $result = $this->process->send($url);
 
         return $result;
     }
@@ -41,46 +41,9 @@ class PhantomDriver {
 
     public function screenshot ($inFile, $outFile) {
 
-        if (!$this->running) {
-
-            // TODO: Throw an exception
-            return;
-        }
-
-        fwrite($this->pipes[0], "$inFile\n");
-
-        $imageBase64 = fgets($this->pipes[1]);
+        $imageBase64 = $this->process("$inFile\n");
         $image = base64_decode($imageBase64);
 
         file_put_contents($outFile, $image);
-    }
-
-    public function start ($script) {
-
-        $descriptorspec = array(
-            0 => array("pipe", "r"),
-            1 => array("pipe", "w"),
-            2 => array("file", "/tmp/error-output.txt", "a")
-        );
-
-        $command = 'phantomjs ' . $script;
-
-        $this->process = proc_open($command, $descriptorspec, $this->pipes);
-        $this->running = true;
-    }
-
-    public function stop () {
-
-        $this->running = false;
-
-        fclose($this->pipes[0]);
-        fclose($this->pipes[1]);
-
-        // It is important that you close any pipes before calling
-        // proc_close in order to avoid a deadlock
-
-        $return_value = proc_close($this->process);
-
-        return $return_value;
     }
 }
---
+++ lib/PhantomProcess.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace NielsHoppe\PhantomJS;
+
+/**
+ *
+ */
+
+class PhantomProcess {
+
+    private $pipes;
+    private $process;
+    private $running;
+    private $command;
+
+    function __construct () {
+
+        $this->running = false;
+        $this->command = 'phantomjs'; // which phantomjs
+    }
+
+    public function start ($script) {
+
+        if (!file_exists($script)) {
+
+            throw new \Exception('No such file: ' . $script);
+        }
+
+        $descriptorspec = array(
+            0 => array("pipe", "r"),
+            1 => array("pipe", "w"),
+            2 => array("pipe", "r")
+        );
+
+        $command = $this->command . ' ' . $script;
+
+        $this->process = proc_open($command, $descriptorspec, $this->pipes);
+        $this->running = true;
+    }
+
+    public function stop () {
+
+        $this->running = false;
+
+        fclose($this->pipes[0]);
+        fclose($this->pipes[1]);
+        fclose($this->pipes[2]);
+
+        // It is important that you close any pipes before calling
+        // proc_close in order to avoid a deadlock
+
+        $return_value = proc_close($this->process);
+
+        return $return_value;
+    }
+
+    public function send ($message) {
+
+        if (!$this->running) {
+
+            throw new \Exception('Process not running.');
+        }
+
+        // send URL to PhantomJS process
+        if (fwrite($this->pipes[0], "$url\n") === false) {
+
+            throw new \Exception('Message not sent for unknown reason.');
+        }
+
+        // read back result from PhantomJS process
+        $result = fgets($this->pipes[1]);
+
+        // read back error from PhantomJS process
+        $error = fgets($this->pipes[2]);
+
+        if ($error) {
+
+            throw new \Exception($error);
+        }
+
+        return $result;
+    }
+}
